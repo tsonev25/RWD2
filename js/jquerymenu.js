c12058122deb943617082681c9ab0304bb35613f
tsonev25 <viktor.tsonev@edu.mon.bg>
2025-02-06 12:58:43 +0200

    Add files via upload

---
+++ js/jquerymenu.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function() {
+    jQuery('.toggle-nav').click(function(e) {
+        jQuery(this).toggleClass('active');
+        jQuery('.menu ul').toggleClass('active');
+        e.preventDefault();
+    });
+});
